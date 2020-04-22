724aae85e80d682725baa9a6eca19624f833296f
Eugene Didenko <ydidenko@mobiquityinc.com>
2020-04-22 15:18:33 +0200

    lalal

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link test', () => {
+test('renders learn react link just testing', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
